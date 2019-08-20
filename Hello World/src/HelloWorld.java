b103363fc398347cb1b8807e2bca060dd46e6c10
APCS3rd <APCS3rd@MHS-LT9-01>
2019-08-20 10:12:15 -0700

    Completed Hello World project.

---
+++ Hello World/src/HelloWorld.java
@@ -0,0 +1,15 @@
+//Wesley Tam
+//August 19, 2019
+//3rd period
+
+//This class displays the text "Hello, World!" to the console window
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello, World!");
+		
+	}
+
+}
