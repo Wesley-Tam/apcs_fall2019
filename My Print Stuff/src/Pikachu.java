67ad2f3607c7971078ffd97455a90817da5f52f1
APCS3rd <APCS3rd@MHS-LT9-01>
2019-08-27 11:36:32 -0700

    Pika Pika

---
+++ My Print Stuff/src/Pikachu.java
@@ -0,0 +1,38 @@
+//Wesley Tam
+//August 27, 2019
+//This class draws Pikachu and uses static methods to simplify the program
+
+public class Pikachu {
+	
+	public static void main (String[] args) {
+		pikachuPrint();
+		drawBox();
+		System.out.println();
+		drawBox();
+		System.out.println();
+		drawBox();
+		System.out.println();
+	}
+	public static void pikachuPrint() {
+		System.out.println("Hello");
+		System.out.println("Pikachu Goes Here");
+		System.out.println("Pikachu welcomes you to the world of Pokemon");
+		System.out.println("\t\t\t (\\__/)");
+		System.out.println("\t\t\t (o^.^)");
+		System.out.println("\t\t\tz(_(\")(\")");
+		
+	}
+	public static void plusMinusPlus() {
+		System.out.println("+-------+");
+	}
+	public static void twoVerticalLines() {
+		System.out.println("|       |");
+	}
+	public static void drawBox() {
+		plusMinusPlus();
+		twoVerticalLines();
+		twoVerticalLines();
+		plusMinusPlus();
+
+	}
+}
