4e5ce9c232e1f17acae1ccfe116543f98c480ac6
APCS3rd <APCS3rd@MHS-LT9-01>
2019-08-29 11:12:02 -0700

    WIP: Accesses Calculate class

---
+++ Access Calculate/src/DoMath.java
@@ -0,0 +1,12 @@
+//Wesley Tam
+//APCS Period 3
+//August 29, 2019
+//This class will call the code form the Calculate class (a.k.a client or runner code)
+
+public class DoMath {
+
+	public static void main(String[] args) {
+
+	}
+
+}
