c79ed4d5e4968fa549026d64a7a0abe42ff79c7b
APCS3rd <APCS3rd@MHS-LT9-01>
2019-08-29 11:10:37 -0700

    WIP Library of math methods and functions

---
+++ Calculate Library/src/Calculate.java
@@ -0,0 +1,12 @@
+//Wesley Tam
+//APCS Period 3
+//August 29, 2019
+//This is a library of math methods and functions
+
+public class Calculate {
+	public static int square(int number) {
+		int answer;
+		answer = number * number;
+		return answer;
+	}
+}
