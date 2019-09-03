e10496f7cc6095811824d72902a241074c1d1f60
APCS3rd <APCS3rd@MHS-LT9-01>
2019-09-03 09:50:40 -0700

    WIP: Library of math methods and functions

--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -4,9 +4,23 @@
 //This is a library of math methods and functions
 
 public class Calculate {
+	
 	public static int square(int number) {
+		//A call to square returns the square of the value passed. The method accepts and integer and returns an integer.
 		int answer;
 		answer = number * number;
 		return answer;
+	}
+	
+	public static int cube(int number) {
+		//A call to cube returns the cube of the value passed. The method accepts and integer and returns an integer.
+		int answer;
+		answer = number * number * number;
+		return answer;
+	}
+		
+	public static double average(double number, double secnum) {
+		//A call to average returns the average of the values passed to it. The method accepts two doubles and returns two doubles.
+		return (number * secnum)/2;
 	}
 }
