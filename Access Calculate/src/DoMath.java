6825aac63e40f663b93675f215fe3ec3f2c25e51
APCS3rd <APCS3rd@MHS-LT9-01>
2019-09-10 10:58:23 -0700

    Tests Math Calculate Library

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -6,11 +6,19 @@
 public class DoMath {
 
 	public static void main(String[] args) {
+		System.out.println(Calculate.square(2));
+		System.out.println(Calculate.cube(2));
 		System.out.println(Calculate.average (2, 10, 10));
 		System.out.println(Calculate.toDegrees(2));
 		System.out.println(Calculate.discriminant(2, 2, 2));
 		System.out.println(Calculate.toImproperFrac(3, 1, 2));
 		System.out.println(Calculate.toMixedNum(7, 2));
 		System.out.println(Calculate.foil(2, 3, 6, -7, "x"));
+		System.out.println(Calculate.isDivisibleBy(3, 2));
+		System.out.println(Calculate.absValue(-5));
+		System.out.println(Calculate.max(-1, -5, 1000));
+		System.out.println(Calculate.min(4, -1));
+		System.out.println(Calculate.round2(5.737));
+		System.out.println(Calculate.exponent(2, 4));
 	}
 }
