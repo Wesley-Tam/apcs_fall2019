b8a0b762e90dff62fa5748217a5b9038ef41828d
APCS3rd <APCS3rd@MHS-LT9-01>
2019-09-19 10:50:06 -0700

    Accesses Calculate Library

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -21,7 +21,7 @@ public class DoMath {
 		System.out.println(Calculate.round2(5.1));
 		System.out.println(Calculate.exponent(2, 4));
 		System.out.println(Calculate.factorial(7));
-		System.out.println(Calculate.isPrime(9));
-		System.out.println(Calculate.gcf(45,  15));
+		System.out.println(Calculate.isPrime(18));
+		System.out.println(Calculate.gcf(40, 15));
 	}
 }
