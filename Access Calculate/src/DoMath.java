9406cd76d0a765c26071a78d57d54389ea10a50b
Wt <Wt@10.0.0.109>
2019-09-20 19:20:37 -0700

    Accesses Calculate Library

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -19,8 +19,8 @@ public class DoMath {
 		System.out.println(Calculate.min(4, -1));
 		System.out.println(Calculate.round2(5.1));
 		System.out.println(Calculate.exponent(2, 4));
-		System.out.println(Calculate.factorial(7));
-		System.out.println(Calculate.isPrime(18));
+		System.out.println(Calculate.factorial(10));
+		System.out.println(Calculate.isPrime(19));
 		System.out.println(Calculate.gcf(45, 6));
 	}
 }
