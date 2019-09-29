2f91cd12ef7b6b6b3eef2a05a456bd8f45163a68
Wt <Wt@Legion2.hsd1.ca.comcast.net>
2019-09-29 13:05:08 -0700

    figure out how to make method repeat at end

--- QuadraticDescriber/src/Quadratic.java
+++ QuadraticDescriber/src/Quadratic.java
@@ -6,18 +6,28 @@
 public class Quadratic {
 	public static String quadrDescriber(double a, double b, double c) {
 		//equation
-		String equation = a + "x^2 + " + b + "x + " + c;
+		String equation = "Equation: " + a + "x^2 + " + b + "x + " + c;
 		//direction
+		String direction = "";
 		if (a < 0) {
-			String direction = "The parabola opens down.";
+			direction = "The parabola opens down.";
 		} else {
-			String direction = "The parabola opens up.";
+			direction = "The parabola opens up.";
 		}
 		//axis of symmetry
 		double xaxis = round2(-b / (2 * a));
-		double ycoord = round2()
+		String axis = "Axis of Symmetry: x = " + xaxis;
+		//vertex
+		double ycoord = round2((a * (xaxis * xaxis)) + (b * xaxis) + c);
+		String vertex = "Vertex: (" + xaxis + ", " + ycoord + ")";
+		//x intercepts
+		String roots = "x-intercept(s): " + quadform(a, b, c);
+		//y-intercepts
+		String yint = "y-intercept: " + c;
+		String console = equation + "\n" + direction + "\n" + axis + "\n" + vertex + "\n" + roots + "\n" + yint;
+		return console;
 	}
-//----------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 	public static double round2(double num) {
 		//A call to round2 rounds a double a double correctly to 2 decimal places and returns a double.
 		num =  (num * 100);
@@ -26,5 +36,70 @@ public class Quadratic {
 		num = num/100;
 		num = (double)num;
 		return num;
+	}
+	
+	public static int min(int num1, int num2) {
+		//A call to min returns the smaller of the values passed. The method accepts two doubles and returns an int.
+		if (num1 < num2) {
+			return num1;
+		} else {
+			return num2;
+		}
+	}
+	
+	public static double max(double num1, double num2) {
+		//A call to max returns the larger of the values passed. The method accepts two doubles and returns a double.
+		if (num1 > num2) {
+			return num1;
+		} else {
+			return num2;
+		}
+	}
+	
+	public static double discriminant(double a, double b, double c) {
+		//A call to discriminant provides the coefficients of a quadratic formula in standard form (a, b, and c) and returns the value of the discriminant. The method accepts three doubles and returns a double.
+		return ((b * b) - (4 * a * c));
+	}
+	
+	public static double absValue(double num) {
+		//A call to absValue returns the absolute value of the number passed. The method accepts a double and returns a double.
+		if (num >= 0) {
+			return num;
+		} else {
+			return (num * -1);
+		}
+	}
+	
+	public static double sqrt(double num) {
+		//A call to sqrt returns an approximation of the square root of the value passed, rounded to two decimal places. the method accepts a double and returns a double.
+		//Done with the help of Caleb Ng
+		if (num < 0) {
+			throw new IllegalArgumentException("Cannot take the square root of a negative number");
+		}
+		double guess = 1;
+		if (num == 0) {
+			guess = 0;
+		}
+		while (absValue((guess * guess) - num) >= (0.005)) {
+			guess = 0.5 * (num / guess + guess);
+		}
+		guess = round2(guess);
+		return guess;
+	}
+	
+	public static String quadform(double a, double b, double c) {
+		//A call to quadform uses the coefficients of a quadratic equation in standard form and uses the quadratic formula to approximate the real roots, if any.
+		if (discriminant(a, b, c) < 0) {
+			return "No x-intercepts";
+		}
+		double root1 = (-b + (sqrt(discriminant(a, b, c)))) / (2 * a);
+		double root2 = (-b - (sqrt(discriminant(a, b, c)))) / (2 * a);
+		if (root1 == root2) {
+			return "(" + round2(root1) + ")";
+		} else {
+			double rootmin = min((int)root1, (int)root2);
+			double rootmax = max(root1, root2);
+			return round2(rootmin) +  " and " + round2(rootmax);
+		}
 	}
 }
--- QuadraticDescriber/src/QuadraticClient.java
+++ QuadraticDescriber/src/QuadraticClient.java
@@ -3,6 +3,24 @@
 //August 26, 2019
 //This is the scanner client that takes in the user's data values and prints out the description.
 
+import java.util.Scanner;
+
 public class QuadraticClient {
-	
+	public static void main(String[] args) {
+		Scanner userInput = new Scanner(System.in);
+		System.out.println("Welcome to the quadratic describer!\nPlease input values for a, b, and c below!");
+		System.out.print("a: ");
+		double a = userInput.nextDouble();
+		System.out.print("b: ");
+		double b = userInput.nextDouble();
+		System.out.print("c: ");
+		double c = userInput.nextDouble();
+		System.out.println();
+		System.out.println(Quadratic.quadrDescriber(a, b, c));
+		System.out.println();
+		System.out.println("Do you want to go again? Type \"quit\" to end, type anything else to continue.");
+		if ((userInput.nextLine()).toLowerCase() == "quit") {
+			
+		}
+	}
 }
