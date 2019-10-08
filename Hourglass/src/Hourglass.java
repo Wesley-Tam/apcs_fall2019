b57608aab364104854667b113318f9c13143c253
APCS3rd <APCS3rd@MHS-LT9-01>
2019-10-08 11:02:16 -0700

    Work on second half

--- Hourglass/src/Hourglass.java
+++ Hourglass/src/Hourglass.java
@@ -11,15 +11,26 @@ public class Hourglass {
 		}
 		System.out.println("|");
 		for (int row = 1; row <= 4; row++) {
-			for (int space = 1; space <= 1; space++) {
+			for (int space = 1; space <= row; space++) {
 				System.out.print(" ");
 			}
 			System.out.print("\\");
-			int numcolon = -2 * row + 10;
-			for (int colon = 1; colon < numcolon; colon++) {
+			int numColon = -2 * row + 10;
+			for (int colon = 0; colon < numColon; colon++) {
 				System.out.print(":");
 			}
 			System.out.println("/");
+		}
+		System.out.println("     ||");
+		for (int row2 = 4; row2 >= 1; row2--) {
+			for (int space2 = row2; space2 >= 1; space2--) {
+				System.out.print(" ");
+			}
+			System.out.print("\\");;
+			int numColon2 = 2 * row2 - 10;
+			for (int colon2 = numColon2; colon2 > 0; colon2--) {
+				System.out.print(":");
+			}
 		}
 	}
 }
