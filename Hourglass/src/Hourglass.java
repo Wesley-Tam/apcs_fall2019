c0bf27212f8fb4725ddd38a47d8f7829109f8544
Wt <Wt@Legion2.hsd1.ca.comcast.net>
2019-10-11 21:24:10 -0700

    finish

--- Hourglass/src/Hourglass.java
+++ Hourglass/src/Hourglass.java
@@ -5,38 +5,29 @@
 
 public class Hourglass {
 	public static void main(String[] args) {
+		int size = 10;
 		System.out.print("|");
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < size; i++) {
 			System.out.print("\"");
 		}
 		System.out.println("|");
-		for (int row = 1; row <= 4; row++) {
+		for (int row = 1; row <= (size / 2 - 1); row++) {
 			for (int space = 1; space <= row; space++) {
 				System.out.print(" ");
 			}
 			System.out.print("\\");
-			int numColon = -2 * row + 10;
-			for (int colon = 0; colon < numColon; colon++) {
+			int numColon = -2 * row + size;
+			for (int colon = 1; colon <= numColon; colon++) {
 				System.out.print(":");
 			}
 			System.out.println("/");
 		}
-		System.out.println("     ||");
-		for (int row2 = 6; row2 <= 9; row2++) {
-			for (int space2 = 9; space2 >= row2; space2++) {
-				System.out.print(" ");
-			}
-			System.out.print("/");
-			int numColon2 = 2 * row2 - 10;
-			for (int colon2 = numColon2; colon2 > 0; colon2--) {
-				System.out.print(":");
-			} 
-			System.out.println("\\");
+		for (int i = 0; i <= (size / 2 - 1); i++) {
+			System.out.print(" ");
 		}
-		System.out.print("|");
-		for (int i = 0; i < 10; i++) {
-			System.out.print("\"");
-		} 
-		System.out.print("|");
+		System.out.println("||");
+		for (int row2 = size / 2 + 1; row2 <= size; row2++) {
+			for (int space2 = )
+		}
 	}
 }
