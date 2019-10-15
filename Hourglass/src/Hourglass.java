916360dacdbc71a9a70bac727e05900ef609b5f7
APCS3rd <APCS3rd@MHS-LT9-01>
2019-10-15 12:23:13 -0700

    Prints an hourglass figure using nested loops

--- Hourglass/src/Hourglass.java
+++ Hourglass/src/Hourglass.java
@@ -3,22 +3,22 @@
 //APCS Period 3
 //This code prints out an hourglass figure using nested loops.
 
-//Your code should print the basic 11 line hourglass. 
-//It should use nested loops and use less than 11 println or prints.  (I can do it with one print.)
-//Export the .java file and submit it.
-	//make it print different sizes
-	//make it repeat for different inputs
-	//deal with any size -- even or odd
-	//make a new method to replace tophalf and bottomhalf
-
 public class Hourglass {
 	public static void main(String[] args) {
-		int size = 101;
+		int size = 11;
 		firstLastLine(size);
 		topHalf(size);
 		middle(size);
 		bottomHalf(size);
 		firstLastLine(size);
+	}
+	
+	public static void space(int x) {	//reduce number of print/printlns
+		System.out.print(" ");
+	}
+	
+	public static void colon(int y) {	//reduce number of print/printlns
+		System.out.print(":");
 	}
 	
 	public static void firstLastLine(int size) {
@@ -32,12 +32,11 @@ public class Hourglass {
 	public static void topHalf(int size) {
 		for (int row = 1; row <= (size / 2 - 1); row++) {
 			for (int space = 1; space <= row; space++) {
-				System.out.print(" ");
+				space(1);
 			}
 			System.out.print("\\");
-			int numColon = -2 * row + size;
-			for (int colon = 1; colon <= numColon; colon++) {
-				System.out.print(":");
+			for (int colon = 1; colon <= (-2 * row + size); colon++) {
+				colon(1);
 			}
 		System.out.println("/");
 		}
@@ -45,7 +44,7 @@ public class Hourglass {
 	
 	public static void middle(int size) {
 		for (int i = 0; i <= (size / 2 - 1); i++) {
-			System.out.print(" ");
+			space(1);
 		}
 		 if (size % 2 == 0) {
 			 System.out.println("||");
@@ -55,14 +54,13 @@ public class Hourglass {
 	}
 	
 	public static void bottomHalf(int size) {
-		for (int row2 = size / 2 + 1; row2 <= size; row2++) {
+		for (int row2 = (size / 2 + 1); row2 <= size; row2++) {
 			for (int space2 = row2; space2 < size; space2++) {
-				System.out.print(" ");
+				space(1);
 			}
 			System.out.print("/");
-			int numColon2 = 2 * row2 - size;
-			for (int colon2 = 0; colon2 < numColon2; colon2++) {
-				System.out.print(":");
+			for (int colon2 = 0; colon2 < (2 * row2 - size); colon2++) {
+				colon(1);
 			}
 			System.out.println("\\");
 		}
