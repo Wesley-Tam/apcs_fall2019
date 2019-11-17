f386efae02e5f3be60c84f8f688fe3986b5b5fe1
Wt <Wt@Legion2.hsd1.ca.comcast.net>
2019-11-17 15:05:01 -0800

    Accesses Calculate Library

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -5,24 +5,24 @@
 
 public class DoMath {
 	public static void main(String[] args) {
-		System.out.println(Calculate.square(2));
-		System.out.println(Calculate.cube(2));
-		System.out.println(Calculate.average (2, 10, 10));
-		System.out.println(Calculate.toDegrees(2));
-		System.out.println(Calculate.discriminant(2, 2, 2));
-		System.out.println(Calculate.toImproperFrac(3, 1, 2));
-		System.out.println(Calculate.toMixedNum(7, 2));
-		System.out.println(Calculate.foil(2, 3, 6, -7, "x"));
-		System.out.println(Calculate.isDivisibleBy(0, 2));
-		System.out.println(Calculate.absValue(-5));
-		System.out.println(Calculate.max(-1, -5, 1000));
-		System.out.println(Calculate.min(4, -1));
-		System.out.println(Calculate.round2(5.1));
-		System.out.println(Calculate.exponent(1, 1));
-		System.out.println(Calculate.factorial(3));
-		System.out.println(Calculate.isPrime(19));
-		System.out.println(Calculate.gcf(45, 6));
-		System.out.println(Calculate.sqrt(0));
-		System.out.println(Calculate.quadForm(1, -6, 9));
+		//System.out.println(Calculate.square(2));
+		//System.out.println(Calculate.cube(2));
+		//System.out.println(Calculate.average (2, 10, 10));
+		//System.out.println(Calculate.toDegrees(2));
+		//System.out.println(Calculate.discriminant(2, 2, 2));
+		//System.out.println(Calculate.toImproperFrac(1000000, 10000000, 19999999));
+		//System.out.println(Calculate.toMixedNum(7, -1));
+		//System.out.println(Calculate.foil(2, 3, 6, -7, "x"));
+		//System.out.println(Calculate.isDivisibleBy(6, 6));
+		//System.out.println(Calculate.absValue(0));
+		//System.out.println(Calculate.max(-1, -5, 1000));
+		//System.out.println(Calculate.min(4, -1));
+		//System.out.println(Calculate.round2(-7.89478));
+		//System.out.println(Calculate.exponent(6, 7));
+		//System.out.println(Calculate.factorial(9));
+		//System.out.println(Calculate.isPrime(1));
+		System.out.println(Calculate.gcf(-1, 8));		//will negative numbers be tested?
+		System.out.println(Calculate.sqrt(347867892));
+		System.out.println(Calculate.quadForm(1, 4, 4));
 	}
 }
